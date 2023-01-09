d6264918662ee89e3a7c5ca35db711476443b4a3
Milos <milos.ilic20@hotmail.com>
2023-01-09 21:46:52 +0100

    projects refactor

--- src/Layout/Projects.tsx
+++ src/Layout/Projects.tsx
@@ -2,18 +2,38 @@ import Project from "../Components/Project";
 
 const Projects = () => {
   return (
-    <section className="project-section" id="projects">
+    <>
       <h2>Projects</h2>
-      <Project
-        name={"Doggo"}
-        description={{
-          shortDescription: "kratka varijanta teksta koja treba da se vidi",
-          longDescription: "duga prica koja moze da ima neki opis i korist",
-        }}
-        image={"nema slike"}
-        links={"google.com"}
-      />
-    </section>
+      <section className="project-section" id="projects">
+        <Project
+          name={"Doggo"}
+          description={{
+            shortDescription: "kratka varijanta teksta koja treba da se vidi",
+            longDescription: "duga prica koja moze da ima neki opis i korist",
+          }}
+          image={"nema slike"}
+          links={"google.com"}
+        />
+        <Project
+          name={"Doggo"}
+          description={{
+            shortDescription: "kratka varijanta teksta koja treba da se vidi",
+            longDescription: "duga prica koja moze da ima neki opis i korist",
+          }}
+          image={"nema slike"}
+          links={"google.com"}
+        />
+        <Project
+          name={"Doggo"}
+          description={{
+            shortDescription: "kratka varijanta teksta koja treba da se vidi",
+            longDescription: "duga prica koja moze da ima neki opis i korist",
+          }}
+          image={"nema slike"}
+          links={"google.com"}
+        />
+      </section>{" "}
+    </>
   );
 };
 
