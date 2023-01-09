7130d7c857245112166e3d2336f19df929bd5096
Milos <milos.ilic20@hotmail.com>
2023-01-09 19:44:19 +0100

    icons group

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -14,6 +14,9 @@ import mongodb from "../assets/mdi_mongodb.svg";
 import git from "../assets/mdi_git.svg";
 import figma from "../assets/mdi_figma.svg";
 
+import javascript2 from "../assets/mdi_language-javascript2.svg";
+
+
 const About = () => {
   return (
     <section className="about-section" id="about">
@@ -49,30 +52,7 @@ const About = () => {
       </p>
 
       <p>to challenge myself and will to learn more</p>
-      <p>
-        Frontend development refers to the practice of building and maintaining
-        the user interface of a website or application. It involves using
-        languages such as HTML, CSS, and JavaScript to create the visual
-        elements and interactions that users see and engage with when they visit
-        a website. One of the most important aspects of frontend development is
-        ensuring that the user experience is intuitive and seamless. This
-        involves careful planning and consideration of how the interface will
-        look and function across different devices and screen sizes. Frontend
-        developers also need to be up-to-date on the latest trends and best
-        practices in web design and development. This includes staying informed
-        about new technologies and frameworks such as React , which
-        can help streamline the development process and create more efficient
-        and effective interfaces. In addition to technical skills, frontend
-        developers also need strong problem-solving and communication skills.
-        They may work closely with designers, project managers, and other team
-        members to ensure that the final product meets the needs and
-        expectations of the client and end users. Overall, frontend development
-        is an essential part of the web development process, and those who excel
-        in this field are in high demand. Whether you are just starting out in
-        your career or are an experienced developer looking to expand your
-        skills, frontend development is a rewarding and challenging field that
-        offers endless opportunities for growth and innovation.
-      </p>
+      
       <div className="card-container">
         <Card
           title="Team player"
@@ -87,8 +67,8 @@ const About = () => {
           body="Open for new projects and suggestions for improvement."
         />
       </div>
-      <div>
-        <img src={html} alt={html} />
+      <div className="icons-container">
+        <img className="svg-icon" src={html} alt={html} />
         <img src={css} alt={css} />
         <img src={javascript} alt={javascript} />
         <img src={typescript} alt={typescript} />
