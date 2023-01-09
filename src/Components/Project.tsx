7369d73358513bab9e795b7aadf561354cfa4d44
Milos <milos.ilic20@hotmail.com>
2023-01-09 22:34:29 +0100

    refactor project links

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import github from "../assets/mdi_github.svg";
 
 type IProject = {
@@ -10,31 +8,25 @@ type IProject = {
 };
 
 const Project = ({ name, description, image, links }: IProject) => {
-  const [isOpened, setIsOpened] = useState(false);
-  const handleClick = () => {
-    // koristi se da se pojavi drugi izgled
-    setIsOpened(!isOpened);
-  };
+
+
   return (
     <article className="project-card">
-      <h4>{name}</h4>
-      <div>{image}</div>
-      <p onClick={handleClick}>
-        {(description = "duzi bla bla tekst ")}
+      <h3>{name}</h3>
+      <div><img src={image} alt={image}/></div>
+      <p>
+        {description }
       </p>
-      {/* da se procita samo malo o projektu, pa kada se klikne na njega da
-            izmeni izgled, bude popup alert sa vise opisa tj teksta 
-        */}
       <p className="project-technologies">Technologies used: </p>
       <ul className="project-list">
         <li>Next.js</li>
         <li>Tailwind.css</li>
       </ul>
-      <div>
+      <div className="project-links">
         <a href={links}>
           <img src={github} alt={github} />
         </a>
-        <a href={links}> demo</a>
+        <a href={links}> see demo</a>
       </div>
     </article>
   );
