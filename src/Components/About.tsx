cb2c4b82a0410ac5489809caf99b79da3e3b22f7
Milos <milos.ilic20@hotmail.com>
2023-01-21 20:34:08 +0100

    about text edit v1

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -35,27 +35,13 @@ const About = () => {
     <section className="about-section" id="about">
       <h2>About me</h2>
       <p>
-       As a self-taught front-end developer, I am excited to present my
-        portfolio to you. My name is Miloš, and I become skilled in the design,
-        development and deployment of visually pleasing and user-friendly
-        websites and web applications. 
-        In this portfolio, you will find examples of my work, showcasing my
-        capabilities and skills as a self-taught web developer. I am eager
-        to take on new challenges and continue to improve my skills and 
-        confident that I can deliver high-quality work.
-        Throughout my journey as a self-taught developer, I have gained valuable
-        experience in understanding user-centered design, web accessibility and
-        responsive design. I am a strong collaborator and communicator, able to
-        understand the needs of my clients and translate them into visually
-        pleasing and functional websites. I have been passionate about web development for quite some time now and
-        taught myself the skills needed to become a proficient front-end
-        developer. I have a strong understanding of web development technologies
-        such as HTML, CSS, JavaScript, TypeScript and front-end frameworks like
-        React, Nextjs. I'm also well-versed in design tools such as Figma.  
-         I am a quick learner and always eager to improve my skills. I have a
-        great attention to detail and dedicated to producing high-quality,
-        scalable and efficient code. I'm always looking for ways to stay
-        up-to-date with the latest web development technologies and trends.
+      As a self-taught front-end web developer, I am happy to present my portfolio to you. I specialize in creating good visually pleasing and user-friendly websites and web applications using a variety of technologies and tools.
+      </p>
+      <p>
+        I am a highly motivated, efficient and detail-oriented.Through my journey as a self-taught developer, I have gained a lot of experience and understanding of client needs. I'm dedicated to the needs of the clients, how I can solve the problem in the best way. I have a good understanding of the needs of web accessibility and responsive design
+      </p>
+      <p>
+      Path as a self-taught front-end developer, I understand the challenges and rewards of self-learning.  I am always looking for ways to improve my skills and stay up-to-date with the latest web development technologies and trends.
       </p>
 
       <div className="card-container">
